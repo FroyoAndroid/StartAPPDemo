adf949a22d4da5353aff6d4b09a05023fe4552e5
NIRAJ KUMAR CHAUHAN <niraj.1991@gmail.com>
2014-12-10 22:14:33 +0530

    Initial Commit

---
+++ Assets/Scripts/GameObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using StartApp;
+
+public class GameObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Debug.Log("Loading ad code");
+		//StartAppWrapper.loadAd();
+		//StartAppWrapper.showAd();
+		#if UNITY_ANDROID
+		   StartAppWrapper.addBanner( 
+           StartAppWrapper.BannerType.AUTOMATIC,
+           StartAppWrapper.BannerPosition.BOTTOM);
+		#endif
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void onReceiveAd()	{
+	
+	}	
+	public void onFailedToReceiveAd()	{
+	
+	}
+}
